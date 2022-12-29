f2bbea7a33515ec12fdb5c86475b1fd3916a9551
tristyn.leos <tristyn.leos@tristynleossMBP.attlocal.net>
2022-12-29 08:53:52 -0600

    update copy

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,10 @@ import pdfMake from "pdfmake/build/pdfmake";
 import pdfFonts from "pdfmake/build/vfs_fonts";
 import htmlToPdfmake from "html-to-pdfmake";
 import { jsPDF } from "jspdf";
+import Loader from "../public/Loader.gif";
+import Loader2 from "../public/Loader.svg";
 import Logo from "../public/logo.png";
-import LogRocket from "logrocket";
+
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 const test = [];
@@ -73,19 +75,6 @@ export default function Home() {
   //     document.getElementById("exportthis").innerHTML = result;
   //   }
   // }, [result]);
-
-  useEffect(() => {
-    if (window.location.href.includes("oddityai")) {
-      LogRocket.init("nzb3qh/oddity-ai");
-      // the below i to identify users when i add auth0
-      // LogRocket.identify("THE_USER_ID_IN_YOUR_APP", {
-      //   name: "James Morrison",
-      //   email: "jamesmorrison@example.com",
-      //   // Add your own custom user variables here, ie:
-      //   subscriptionType: "pro",
-      // });
-    }
-  }, []);
 
   return (
     <div>
