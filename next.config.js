dfeb9a42590c3c496a6293036405d7a84af69481
tristyn.leos <tristyn.leos@tristynleossMBP.attlocal.net>
2022-12-29 12:10:19 -0600

    fix next js

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  reactStrictMode: true,
+  env: {
+    REACT_APP_LOGROCKET_API_KEY: process.env.REACT_APP_LOGROCKET_API_KEY,
+    REACT_APP_GOOGLE_ANALYTICS_API_KEY:
+      process.env.REACT_APP_GOOGLE_ANALYTICS_API_KEY,
+  },
+};
