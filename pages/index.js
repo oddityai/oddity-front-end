2549d86b3c663606a5074a2599c39afd7733a384
tristyn.leos <tristyn.leos@tristynleossMBP.attlocal.net>
2023-01-02 14:27:29 -0600

    d

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,14 @@ import { useState, useEffect } from "react";
 import App from "./App";
 import ReactGA from "react-ga4";
 import LogRocket from "logrocket";
+import { hotjar } from "react-hotjar";
 
 const Index = () => {
   useEffect(() => {
+    hotjar.initialize("3307089", "6");
     if (window.location.href.includes("oddityai")) {
       LogRocket.init(process.env.REACT_APP_LOGROCKET_API_KEY);
-      ReactGA.initialize("G-EYH3FLZ9FN");
+      ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_API_KEY);
       // the below i to identify users when i add auth0
       // LogRocket.identify("THE_USER_ID_IN_YOUR_APP", {
       //   name: "James Morrison",
