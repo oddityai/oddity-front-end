d2842f69ee71304cee18aef116a56e772345f46e
tristyn.leos <tristyn.leos@tristynleossMBP.attlocal.net>
2023-01-03 00:16:07 -0600

    ui update

--- pages/App.js
+++ pages/App.js
@@ -219,6 +219,10 @@ export default function Home() {
           <h2 className={nunito.className} style={{ fontSize: 22 }}>
             Use AI to get straight A's in class
           </h2>
+          <h3 className={nunito.className} style={{ fontSize: 18 }}>
+            Choose one of our AI bots below and get answers to your homework.
+            Each AI is specially designed for each subject.
+          </h3>
 
           <Buttons handleClick={handleClick} />
 
--- pages/AppBar.js
+++ pages/AppBar.js
@@ -1,4 +1,4 @@
-import * as React from "react";
+import { useEffect, useState } from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
@@ -10,23 +10,46 @@ import TwitterIcon from "@mui/icons-material/Twitter";
 import SmartToyIcon from "@mui/icons-material/SmartToy";
 import { Nunito } from "@next/font/google";
 import Link from "next/link";
+import HamburgerMenu from "./HamburgerMenu";
 
 const nunito = Nunito({ subsets: ["latin"] });
 
 export default function ButtonAppBar() {
+  const getWindowSize = () => {
+    if (typeof window !== "undefined") {
+      // Client-side-only code
+      const { innerWidth, innerHeight } = window;
+      return { innerWidth, innerHeight };
+    }
+  };
+  const [windowSize, setWindowSize] = useState(getWindowSize());
+  useEffect(() => {
+    function handleWindowResize() {
+      setWindowSize(getWindowSize());
+    }
+
+    window.addEventListener("resize", handleWindowResize);
+
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, []);
   return (
     <div
       className={nunito.className}
       style={{
         backgroundColor: "white",
+        borderBottom: "1px solid silver",
         width: "100%",
       }}
     >
       <div
         style={{
-          padding: 16,
+          margin: 8,
           justifyContent: "space-between",
           display: "flex",
+          justifyItems: "center",
+          alignContent: "center",
           alignItems: "center",
         }}
       >
@@ -37,73 +60,116 @@ export default function ButtonAppBar() {
             alignItems: "center",
           }}
         >
-          <img style={{ height: 30, marginRight: 8 }} src="/logo.png" />
-          <div>OddityAI.com</div>
+          <img
+            style={{ height: 30, marginRight: 8, textDecoration: "none" }}
+            src="/logo.png"
+          />
+          <div style={{ fontSize: 18 }}> OddityAI.com</div>
         </div>
         <div>
           <div
             style={{
               padding: 16,
               display: "flex",
+              justifyItems: "center",
               justifyContent: "space-between",
             }}
           >
-            {/* <div style={{ marginRight: 8 }}>
-              <Link href="/home">
+            {/* <div style={{ marginRight: 8, textDecoration: 'none' }}>
+              <Link style={{ textDecoration: 'none', marginLeft: 8, color: 'black' }} href="/home">
                 {" "}
                 <div
                   style={{
                     textDecoration: "none",
+                    textTransform: 'none',
                     color: "black",
-                    fontSize: 18,
+  16                 marginLeft: 8, fontSize: 18,
                   }}
                 >
                   Home
                 </div>
               </Link>
             </div> */}
-            <div style={{ marginRight: 8 }}>
-              <Link href="/">
+            <div style={{ marginRight: 8, textDecoration: "none" }}>
+              <Link
+                style={{
+                  textDecoration: "none",
+                  marginLeft: 8,
+                  color: "black",
+                }}
+                href="/home"
+              >
                 {" "}
-                <div
-                  style={{
-                    textDecoration: "none",
-                    color: "black",
-                    fontSize: 18,
-                  }}
-                >
-                  App
-                </div>
+                Home
               </Link>
             </div>
-            {/* <div style={{ marginRight: 8 }}>
-              <Link href="/about">
+            <div style={{ marginRight: 8, textDecoration: "none" }}>
+              <Link
+                style={{
+                  textDecoration: "none",
+                  marginLeft: 8,
+                  color: "black",
+                }}
+                href="/"
+              >
+                {" "}
+                App
+              </Link>
+            </div>
+            {/* <div style={{ marginRight: 8, textDecoration: 'none' }}>
+              <Link style={{ textDecoration: 'none', marginLeft: 8, color: 'black' }} href="/about">
                 {" "}
                 <div
                   style={{
                     textDecoration: "none",
+                    textTransform: 'none',
                     color: "black",
-                    fontSize: 18,
+  16                 marginLeft: 8, fontSize: 18,
                   }}
                 >
                   About
                 </div>
               </Link>
             </div> */}
-            <div style={{ marginRight: 8 }}>
-              <Link href="/contact">
+            <div style={{ marginRight: 8, textDecoration: "none" }}>
+              <Link
+                style={{
+                  textDecoration: "none",
+                  marginLeft: 8,
+                  color: "black",
+                }}
+                href="/contact"
+              >
                 {" "}
-                <div
-                  style={{
-                    textDecoration: "none",
-                    color: "black",
-                    fontSize: 18,
-                  }}
-                >
-                  Contact
-                </div>
+                Contact
               </Link>
             </div>
+            {/* <div style={{ marginRight: 8, textDecoration: "none" }}>
+              <Link
+                style={{
+                  textDecoration: "none",
+                  marginLeft: 8,
+                  color: "black",
+                }}
+                href="/contact"
+              >
+                {" "}
+                Login
+              </Link>
+            </div> */}
+            {/* <div style={{ marginRight: 8, textDecoration: "none" }}>
+              <Link
+                style={{
+                  textDecoration: "none",
+                  marginLeft: 8,
+                  color: "black",
+                }}
+                href="/contact"
+              >
+                {" "}
+                Signup
+              </Link>
+            </div> */}
           </div>
         </div>
       </div>
