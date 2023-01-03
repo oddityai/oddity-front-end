941ce5daa98cbfe867c5d93e33654058e32c9e8e
tristyn.leos <tristyn.leos@tristynleossMBP.attlocal.net>
2023-01-02 20:13:58 -0600

    add pages

--- pages/AppBar.js
+++ pages/AppBar.js
@@ -9,6 +9,7 @@ import MenuIcon from "@mui/icons-material/Menu";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import SmartToyIcon from "@mui/icons-material/SmartToy";
 import { Nunito } from "@next/font/google";
+import Link from "next/link";
 
 const nunito = Nunito({ subsets: ["latin"] });
 
@@ -38,6 +39,72 @@ export default function ButtonAppBar() {
         >
           <img style={{ height: 30, marginRight: 8 }} src="/logo.png" />
           <div>OddityAI.com</div>
+        </div>
+        <div>
+          <div
+            style={{
+              padding: 16,
+              display: "flex",
+              justifyContent: "space-between",
+            }}
+          >
+            {/* <div style={{ marginRight: 8 }}>
+              <Link href="/home">
+                {" "}
+                <div
+                  style={{
+                    textDecoration: "none",
+                    color: "black",
+                    fontSize: 18,
+                  }}
+                >
+                  Home
+                </div>
+              </Link>
+            </div> */}
+            <div style={{ marginRight: 8 }}>
+              <Link href="/">
+                {" "}
+                <div
+                  style={{
+                    textDecoration: "none",
+                    color: "black",
+                    fontSize: 18,
+                  }}
+                >
+                  App
+                </div>
+              </Link>
+            </div>
+            {/* <div style={{ marginRight: 8 }}>
+              <Link href="/about">
+                {" "}
+                <div
+                  style={{
+                    textDecoration: "none",
+                    color: "black",
+                    fontSize: 18,
+                  }}
+                >
+                  About
+                </div>
+              </Link>
+            </div> */}
+            <div style={{ marginRight: 8 }}>
+              <Link href="/contact">
+                {" "}
+                <div
+                  style={{
+                    textDecoration: "none",
+                    color: "black",
+                    fontSize: 18,
+                  }}
+                >
+                  Contact
+                </div>
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
     </div>
---
+++ pages/about.js
@@ -0,0 +1,67 @@
+import AppBar from "./AppBar";
+import { Nunito } from "@next/font/google";
+
+const nunito = Nunito({ subsets: ["latin"] });
+const About = () => {
+  return (
+    <div className={nunito.className}>
+      <AppBar />
+      <div>
+        <div
+          style={{
+            padding: 16,
+            display: "flex",
+            justifyContent: "space-around",
+            flexDirection: "row",
+            flexWrap: "wrap",
+            fontSize: 18,
+          }}
+        >
+          <div
+            style={{
+              padding: 16,
+              width: "50%",
+              maxWidth: 400,
+            }}
+          >
+            <span style={{ fontWeight: 600 }}>Thank you for visiti</span>
+            <br />
+            <br />
+            We hope that our advanced artificial intelligence has been able to
+            assist you with all of your homework needs.
+            <br />
+            <br />
+            If you have any questions, concerns or feedback, we would love to
+            hear from you. You can reach us through our Facebook{" "}
+            <span
+              style={{ color: "#0a99f2", cursor: "pointer" }}
+              onClick={() => {
+                window.open(
+                  "https://www.facebook.com/profile.php?id=100088926106665"
+                );
+              }}
+            >
+              here
+            </span>{" "}
+          </div>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              position: "relative",
+              width: "50%",
+              maxWidth: 400,
+            }}
+          >
+            <img
+              src="/contact.png"
+              style={{ maxHeight: "100%", maxWidth: "100%" }}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default About;
