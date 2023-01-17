05d0bff40e5d520e31aeb00a2956f5eb43803886
tristyn.leos <tristyn.leos@tristynleossMBP.attlocal.net>
2023-01-17 02:59:48 -0600

    route login/signups to app

--- pages/api/auth/[...auth0].js
+++ pages/api/auth/[...auth0].js
@@ -4,4 +4,9 @@ export default handleAuth({
   signup: handleLogin({
     authorizationParams: { screen_hint: "signup" },
   }),
+  async login(req, res) {
+    await handleLogin(req, res, {
+      returnTo: "/app",
+    });
+  },
 });
