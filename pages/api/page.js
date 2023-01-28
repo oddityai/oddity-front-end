24d630c1e02c229f4fb7d485e101178ff4dc15a6
Studio X IT Loaner1 <studioxitloaner1@Studios-MBP.attlocal.net>
2023-01-28 12:52:43 -0600

    d

--- pages/api/page.js
+++ pages/api/page.js
@@ -26,7 +26,7 @@ export default async function (req, res) {
       model: "text-davinci-003",
       prompt: prompt,
       temperature: 1,
-      max_tokens: 3200,
+      max_tokens: 3000,
       top_p: 1,
       frequency_penalty: 0,
       presence_penalty: 0,
