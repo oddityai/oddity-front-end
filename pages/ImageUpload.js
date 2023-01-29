a5bce1650119645762a7e69803d0b5551f59399e
Studio X IT Loaner1 <studioxitloaner1@Studios-MBP.attlocal.net>
2023-01-29 01:44:50 -0600

    d

--- pages/ImageUpload.js
+++ pages/ImageUpload.js
@@ -192,102 +192,91 @@ const ImageUploadModal = ({
       >
         <ImageIcon />
       </IconButton>
-      <div
-        style={{
-          padding: 16,
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-        }}
-      >
-        <Dialog scroll="paper" open={isModalOpen}>
-          <DialogContent>
-            <DialogContentText id="scroll-dialog-description">
-              <div
-                onClick={() => {
-                  !isProcessing && setIsModalOpen(false);
-                }}
-                style={{
-                  display: "flex",
-                  justifyContent: "flex-end",
-                  cursor: "pointer",
-                  padding: 16,
-                }}
-              >
-                <CloseIcon />
-              </div>
-              <div
-                style={{
-                  padding: 16,
-                  display: "flex",
-                  flexDirection: "column",
-                  justifyContent: "center",
-                }}
-              >
-                <br />
-                {step === 1 && (
-                  <img src={url} style={{ width: "80%", marginLeft: "10%" }} />
-                )}
-                {step === 2 && (
-                  <p
-                    style={{ color: "gray", fontSize: 16, fontWeight: "bold" }}
-                  >
-                    Picture-to-text complete: <mark>{input}</mark>
-                  </p>
-                )}
-                {step === 3 && (
-                  <p
-                    style={{ color: "gray", fontSize: 16, fontWeight: "bold" }}
-                  >
-                    Questions Identified: {rawText}
-                  </p>
-                )}
-                {step === 4 &&
-                  results.map((result, i) => {
-                    return (
-                      <p
-                        style={{
-                          color: "gray",
-                          fontSize: 16,
-                          fontWeight: "bold",
-                        }}
-                      >
-                        {i + 1}. {result.result}
-                      </p>
-                    );
-                  })}
-                {step === 4 && (
-                  <img src={url} style={{ width: "80%", marginLeft: "10%" }} />
-                )}
-                {error && (
-                  <>
-                    <p style={{ color: "red", fontSize: 16 }}>{error}</p>
-                    <p style={{ color: "red", fontSize: 16 }}>
-                      Try: Upload only half a page at a time
+
+      <Dialog scroll="paper" open={isModalOpen}>
+        <DialogContent>
+          <DialogContentText id="scroll-dialog-description">
+            <div
+              onClick={() => {
+                !isProcessing && setIsModalOpen(false);
+              }}
+              style={{
+                display: "flex",
+                justifyContent: "flex-end",
+                cursor: "pointer",
+                padding: 16,
+              }}
+            >
+              <CloseIcon />
+            </div>
+            <div
+              style={{
+                padding: 16,
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+              }}
+            >
+              <br />
+              {step === 1 && (
+                <img src={url} style={{ width: "80%", marginLeft: "10%" }} />
+              )}
+              {step === 2 && (
+                <p style={{ color: "gray", fontSize: 16, fontWeight: "bold" }}>
+                  Picture-to-text complete: <mark>{input}</mark>
+                </p>
+              )}
+              {step === 3 && (
+                <p style={{ color: "gray", fontSize: 16, fontWeight: "bold" }}>
+                  Questions Identified: {rawText}
+                </p>
+              )}
+              {step === 4 &&
+                results.map((result, i) => {
+                  return (
+                    <p
+                      style={{
+                        color: "gray",
+                        fontSize: 16,
+                        fontWeight: "bold",
+                      }}
+                    >
+                      {i + 1}. {result.result}
                     </p>
-                    <p style={{ color: "red", fontSize: 16 }}>
-                      Make sure the image is clear, it's technology not magic.
-                    </p>
-                  </>
-                )}
-                <Button
-                  style={{
-                    zIndex: 10,
-                    backgroundColor: "#304FFD",
-                    padding: 14,
-                    marginBottom: 16,
-                    color: "white",
-                    width: "100%",
-                  }}
-                  onClick={() => handleInputClick("reply")}
-                >
-                  {steps[step]}
-                </Button>
-              </div>
-            </DialogContentText>
-          </DialogContent>
-        </Dialog>
-        {/* <p style={{ fontSize: 18, textAlign: "center", fontWeight: 600 }}>
+                  );
+                })}
+              {step === 4 && (
+                <img src={url} style={{ width: "80%", marginLeft: "10%" }} />
+              )}
+              {error && (
+                <>
+                  <p style={{ color: "red", fontSize: 16 }}>{error}</p>
+                  <p style={{ color: "red", fontSize: 16 }}>
+                    Try: Upload only half a page at a time
+                  </p>
+                  <p style={{ color: "red", fontSize: 16 }}>
+                    Make sure the image is clear, it's technology not magic.
+                  </p>
+                </>
+              )}
+              <Button
+                style={{
+                  zIndex: 10,
+                  backgroundColor: "#304FFD",
+                  padding: 14,
+                  marginBottom: 16,
+                  color: "white",
+                  width: "100%",
+                }}
+                onClick={() => handleInputClick("reply")}
+              >
+                {steps[step]}
+              </Button>
+            </div>
+          </DialogContentText>
+        </DialogContent>
+      </Dialog>
+      {/* <p style={{ fontSize: 18, textAlign: "center", fontWeight: 600 }}>
           Please, only upload ONE question at a time.
         </p>
         <br />
@@ -303,7 +292,6 @@ const ImageUploadModal = ({
         <img src="/x-mark.png" style={{ width: "40%", marginLeft: "30%" }} />
         <br />
         <br /> */}
-      </div>
       <form onSubmit={generateFirebaseUrl}>
         <input
           style={{ display: "hidden", height: 0, width: 0 }}
