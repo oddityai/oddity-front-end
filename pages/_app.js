8aeecca054aae6bc476745b9289bbd9c1906f250
Studio X IT Loaner1 <studioxitloaner1@Studios-MBP.attlocal.net>
2023-02-08 19:12:14 -0600

    d

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 // pages/_app.js
 import React from "react";
 import { UserProvider } from "@auth0/nextjs-auth0/client";
-import 'regenerator-runtime/runtime'
+import "regenerator-runtime/runtime";
 
 export default function App({ Component, pageProps }) {
   return (
