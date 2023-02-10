ed9f5ef9cd23884afe804ff2d92028155ded1e3c
Studio X IT Loaner1 <studioxitloaner1@Studios-MBP.attlocal.net>
2023-02-10 15:31:21 -0600

    d

--- pages/App.js
+++ pages/App.js
@@ -23,7 +23,6 @@ import Hotjar from "@hotjar/browser";
 import { useUser } from "@auth0/nextjs-auth0/client";
 import { db } from "../firebase";
 import Tesseract from "tesseract.js";
-import { Html, Head, Main, NextScript } from "next/document";
 
 import Tabs from "./Tabs";
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
@@ -313,24 +312,6 @@ export default function Home() {
 
   return (
     <div style={{ display: "flex", flexDirection: "column" }}>
-      <Head>
-        <title>AI Homework Helper | Homework AI</title>
-        <meta
-          name="description"
-          content="Homework AI Is the AI That Does Homework. If You
-Are a Student Who Needs Homework Solutions This AI Homework Helper
-Is for You. Give This AI Homework App a Try, It’ll Solve & Write Your
-Homework"
-        />
-        <link rel="shortcut icon" href="/favicon.ico" />
-        <meta
-          name="keywords"
-          content="student homework app ai, ai that does homework, ai doing
-homework, ai homework writer, homework helper ai, homework ai, ai
-homework solver, ai for homework, ai  homework, ai homework solutions, ai
-homework helper"
-        />{" "}
-      </Head>
       <AppBar />
 
       <div>
