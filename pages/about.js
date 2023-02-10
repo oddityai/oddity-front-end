4968a37007dac82852ebcd735690987a176b8720
Studio X IT Loaner1 <studioxitloaner1@Studios-MBP.attlocal.net>
2023-02-10 15:29:46 -0600

    d

--- pages/about.js
+++ pages/about.js
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 
 import AppBar from "./AppBar";
 import { Nunito } from "@next/font/google";
-import { Html, Head, Main, NextScript } from "next/document";
 
 const nunito = Nunito({ subsets: ["latin"] });
 const About = () => {
@@ -32,24 +31,6 @@ const About = () => {
 
   return (
     <div className={nunito.className}>
-      <Head>
-        <title>AI Homework Helper | Homework AI</title>
-        <meta
-          name="description"
-          content="Homework AI Is the AI That Does Homework. If You
-Are a Student Who Needs Homework Solutions This AI Homework Helper
-Is for You. Give This AI Homework App a Try, It’ll Solve & Write Your
-Homework"
-        />
-        <link rel="shortcut icon" href="/favicon.ico" />
-        <meta
-          name="keywords"
-          content="student homework app ai, ai that does homework, ai doing
-homework, ai homework writer, homework helper ai, homework ai, ai
-homework solver, ai for homework, ai  homework, ai homework solutions, ai
-homework helper"
-        />{" "}
-      </Head>
       <AppBar />
       <div>
         <div
