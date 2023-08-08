36286b98dda2eb88d64df05e4af8ec927961eedf
Josh Claxton <jrccode@outlook.com>
2023-08-07 22:16:42 -0500

    ip fix #2

--- pages/api/ip.js
+++ pages/api/ip.js
@@ -1,6 +1,11 @@
-// pages/api/ip.js
 export default function handler(req, res) {
   console.log('Headers:', req.headers)
-  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
-  res.status(200).json({ ip })
+
+  const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress
+
+  // Since x-forwarded-for can contain a comma-separated list of IPs (if passed through multiple proxies),
+  // we'll split it and take the first non-empty value.
+  const firstIp = ip.split(',')[0].trim()
+
+  res.status(200).json({ ip: firstIp })
 }
