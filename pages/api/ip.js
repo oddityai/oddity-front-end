09687e5c1ad5f0cf0322de3b79e5f6903b6d9715
Josh Claxton <jrccode@outlook.com>
2023-08-07 22:11:33 -0500

    ip config fix for non-local ip's

--- pages/api/ip.js
+++ pages/api/ip.js
@@ -1,9 +1,6 @@
 // pages/api/ip.js
 export default function handler(req, res) {
   console.log('Headers:', req.headers)
-  const ip =
-    // req.headers['x-forwarded-for']
-    // ||
-    req.connection.remoteAddress
+  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
   res.status(200).json({ ip })
 }
