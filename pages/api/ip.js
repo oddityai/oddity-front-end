01768757aaa216570535db96221f69bc77edc030
Josh Claxton <jrccode@outlook.com>
2023-08-07 22:02:57 -0500

    ip config testing

--- pages/api/ip.js
+++ pages/api/ip.js
@@ -1,5 +1,9 @@
 // pages/api/ip.js
 export default function handler(req, res) {
-  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
+  console.log('Headers:', req.headers)
+  const ip =
+    // req.headers['x-forwarded-for']
+    // ||
+    req.connection.remoteAddress
   res.status(200).json({ ip })
 }
