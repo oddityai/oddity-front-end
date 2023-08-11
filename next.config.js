b8c4130e730c895435ebb4380d07112ccfcba8cf
Josh Claxton <jrccode@outlook.com>
2023-08-10 23:28:14 -0500

    remove sentry

--- next.config.js
+++ next.config.js
@@ -1,40 +1,8 @@
-const { withSentryConfig } = require('@sentry/nextjs')
-
-module.exports = withSentryConfig(
-  module.exports,
-  {
-    // For all available options, see:
-    // https://github.com/getsentry/sentry-webpack-plugin#options
-
-    // Suppresses source map uploading logs during build
-    silent: true,
-
-    org: 'oddityai',
-    project: 'oddityai',
-    reactStrictMode: true,
-    env: {
-      REACT_APP_LOGROCKET_API_KEY: process.env.REACT_APP_LOGROCKET_API_KEY,
-      REACT_APP_GOOGLE_ANALYTICS_API_KEY:
-        process.env.REACT_APP_GOOGLE_ANALYTICS_API_KEY,
-    },
+module.exports = {
+  reactStrictMode: true,
+  env: {
+    REACT_APP_LOGROCKET_API_KEY: process.env.REACT_APP_LOGROCKET_API_KEY,
+    REACT_APP_GOOGLE_ANALYTICS_API_KEY:
+      process.env.REACT_APP_GOOGLE_ANALYTICS_API_KEY,
   },
-  {
-    // For all available options, see:
-    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-    // Upload a larger set of source maps for prettier stack traces (increases build time)
-    widenClientFileUpload: true,
-
-    // Transpiles SDK to be compatible with IE11 (increases bundle size)
-    transpileClientSDK: true,
-
-    // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
-    tunnelRoute: '/monitoring',
-
-    // Hides source maps from generated client bundles
-    hideSourceMaps: true,
-
-    // Automatically tree-shake Sentry logger statements to reduce bundle size
-    disableLogger: true,
-  }
-)
+}
