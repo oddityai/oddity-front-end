f4ca506a12a9dfd45cfbcb34f1f154cbed893c23
Josh Claxton <jrccode@outlook.com>
2023-08-10 23:18:19 -0500

    sentry fix

--- next.config.js
+++ next.config.js
@@ -1,67 +1,4 @@
-module.exports = {}
-// This file sets a custom webpack configuration to use your Next.js app
-// with Sentry.
-// https://nextjs.org/docs/api-reference/next.config.js/introduction
-
 const { withSentryConfig } = require('@sentry/nextjs')
-
-// your existing module.exports or default export
-const nextConfig = {
-  reactStrictMode: true,
-  env: {
-    REACT_APP_LOGROCKET_API_KEY: process.env.REACT_APP_LOGROCKET_API_KEY,
-    REACT_APP_GOOGLE_ANALYTICS_API_KEY:
-      process.env.REACT_APP_GOOGLE_ANALYTICS_API_KEY,
-  },
-
-  // Optional build-time configuration options
-  sentry: {
-    // See the sections below for information on the following options:
-    //   'Configure Source Maps':
-    disableServerWebpackPlugin,
-    disableClientWebpackPlugin,
-    //     - hideSourceMaps
-    //     - widenClientFileUpload
-    //   'Configure Legacy Browser Support':
-    //     - transpileClientSDK
-    //   'Configure Serverside Auto-instrumentation':
-    //     - autoInstrumentServerFunctions
-    //     - excludeServerRoutes
-    //   'Configure Tunneling to avoid Ad-Blockers':
-    //     - tunnelRoute
-  },
-}
-
-const sentryWebpackPluginOptions = {
-  // Additional config options for the Sentry Webpack plugin. Keep in mind that
-  // the following options are set automatically, and overriding them is not
-  // recommended:
-  //   release, url, configFile, stripPrefix, urlPrefix, include, ignore
-
-  org: 'example-org',
-  project: 'example-project',
-
-  // An auth token is required for uploading source maps.
-  // You can get an auth token from https://sentry.io/settings/account/api/auth-tokens/
-  // The token must have `project:releases` and `org:read` scopes for uploading source maps
-  authToken: process.env.SENTRY_AUTH_TOKEN,
-
-  silent: true, // Suppresses all logs
-
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options.
-}
-
-// Make sure adding Sentry options is the last code to run before exporting
-module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions)
-
-// If you're using a next.config.mjs file:
-// export default withSentryConfig(nextConfig, sentryWebpackPluginOptions);
-
-
-// Injected content via Sentry wizard below
-
-const { withSentryConfig } = require("@sentry/nextjs");
 
 module.exports = withSentryConfig(
   module.exports,
@@ -72,8 +9,14 @@ module.exports = withSentryConfig(
     // Suppresses source map uploading logs during build
     silent: true,
 
-    org: "oddityai",
-    project: "oddityai",
+    org: 'oddityai',
+    project: 'oddityai',
+    reactStrictMode: true,
+    env: {
+      REACT_APP_LOGROCKET_API_KEY: process.env.REACT_APP_LOGROCKET_API_KEY,
+      REACT_APP_GOOGLE_ANALYTICS_API_KEY:
+        process.env.REACT_APP_GOOGLE_ANALYTICS_API_KEY,
+    },
   },
   {
     // For all available options, see:
@@ -86,7 +29,7 @@ module.exports = withSentryConfig(
     transpileClientSDK: true,
 
     // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
-    tunnelRoute: "/monitoring",
+    tunnelRoute: '/monitoring',
 
     // Hides source maps from generated client bundles
     hideSourceMaps: true,
@@ -94,4 +37,4 @@ module.exports = withSentryConfig(
     // Automatically tree-shake Sentry logger statements to reduce bundle size
     disableLogger: true,
   }
-);
+)
