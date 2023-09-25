de1fa3a3093fe8e6a1e29e9a4346d0a4499b2ad8
Tristyn Leos <tristyn.leos@syky.com>
2023-09-25 02:28:57 -0500

    d

--- pages/api/checkout_session_subscribe.js
+++ pages/api/checkout_session_subscribe.js
@@ -3,32 +3,30 @@ const stripe = require("stripe")(
 );
 
 export default async function handler(req, res) {
-  console.log("REQ IS: ", {req})
-  if (req.method === 'POST') {
+  const userId = req.session.userId || req.tokenPayload.userId; 
+  if (req.method === "POST") {
     try {
-      // Create Checkout Sessions from body params.
       const session = await stripe.checkout.sessions.create({
         line_items: [
           {
-            // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
             price: "price_1Nu7vjDeCpRNgE7AmTOtrpYK",
             quantity: 1,
           },
         ],
         mode: "subscription",
         metadata: {
-          user_id: `109917158928421903721`,
+          user_id: userId,
         },
         success_url: `${req.headers.origin}/App?success=true4`,
         cancel_url: `${req.headers.origin}/App?canceled=true`,
         automatic_tax: { enabled: true },
       });
-      res.redirect(303, session.url)
+      res.redirect(303, session.url);
     } catch (err) {
-      res.status(err.statusCode || 500).json(err.message)
+      res.status(err.statusCode || 500).json(err.message);
     }
   } else {
-    res.setHeader('Allow', 'POST')
-    res.status(405).end('Method Not Allowed')
+    res.setHeader("Allow", "POST");
+    res.status(405).end("Method Not Allowed");
   }
 }
--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -1,5 +1,8 @@
 // /pages/api/webhook.js
 import { buffer } from "micro";
+import { db } from "../../firebase";
+
+
 const stripe = require("stripe")("whsec_DZuHPHxwsCr85H4gFNbfpW7W9uapyGMN");
 
 export const config = {
@@ -11,9 +14,7 @@ export const config = {
 async function handler(req, res) {
   const buf = await buffer(req);
   const sig = req.headers["stripe-signature"];
-
-const endpointSecret =
-  "whsec_7f14bfc5f7a62f64661e77b3ac5e1a8daac17f4d7b5e1f720fd93ad0d1239750";
+  const endpointSecret = "whsec_DZuHPHxwsCr85H4gFNbfpW7W9uapyGMN";
 
   let event;
 
@@ -23,32 +24,35 @@ const endpointSecret =
     return res.status(400).send(`Webhook Error: ${err.message}`);
   }
 
-if (event.type === "checkout.session.completed") {
-  const session = event.data.object;
-  const subscriptionId = session.subscription;
-  const userId = session.metadata.user_id;
+  if (event.type === "checkout.session.completed") {
+    const session = event.data.object;
+    const subscriptionId = session.subscription;
+    const userId = session.metadata.user_id;
 
-  const usersRef = db.collection("profiles");
-  const userRef = usersRef.doc(userId);
+    const usersRef = db.collection("profiles");
+    const userRef = usersRef.doc(userId);
 
-  try {
-    await userRef.update({
-      subscribed: true,
-      subscriptionId: subscriptionId,
-    });
+    try {
+      await userRef.update({
+        subscribed: true,
+        subscriptionId: subscriptionId,
+      });
 
-    console.log(`User subscribed. subscriptionId: ${subscriptionId}`);
-  } catch (error) {
-    console.log("Error updating user:", error);
+      console.log(`User subscribed. subscriptionId: ${subscriptionId}`);
+    } catch (error) {
+      console.log("Error updating user:", error);
+    }
+
+    return res
+      .status(200)
+      .json({
+        received: true,
+        subscriptionId: subscriptionId,
+        session: session,
+      });
   }
 
-  return res
-    .status(200)
-    .json({ received: true, subscriptionId: subscriptionId, session: session });
-}
-
-
-  return res.status(200).json({ received: true, session: session });
+  return res.status(200).json({ received: true });
 }
 
 export default handler;
